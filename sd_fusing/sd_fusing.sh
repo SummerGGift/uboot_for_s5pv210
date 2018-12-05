c6e86755463214ca4e9992213437bfc9f1fcebaa
SummerGift <SummerGift@qq.com>
2018-12-05 22:06:12 +0800

    【修改】：sd 烧录脚本

--- sd_fusing/sd_fusing.sh
+++ sd_fusing/sd_fusing.sh
@@ -9,6 +9,7 @@
 ####################################
 reader_type1="/dev/sdb"
 reader_type2="/dev/mmcblk0"
+sd_device=""
 
 if [ -z $1 ]
 then
@@ -16,27 +17,8 @@ then
     exit 0
 fi
 
-if [ $1 = $reader_type1 ]
-then 
-    partition1="$11"
-    partition2="$12"
-    partition3="$13"
-    partition4="$14"
-
-elif [ $1 = $reader_type2 ]
-then 
-    partition1="$1p1"
-    partition2="$1p2"
-    partition3="$1p3"
-    partition4="$1p4"
-
-else
-    echo "Unsupported SD reader"
-    exit 0
-fi
-
-if [ -b $1 ]
-then
+if [ $1 = $reader_type1 ]||[ $1 = $reader_type2 ];then 
+	sd_device="$11"
     echo "$1 reader is identified."
 else
     echo "$1 is NOT identified."
@@ -53,29 +35,10 @@ rm sd_mbr.dat
  
 ####################################
 # format
-umount $partition1 2> /dev/null
-umount $partition2 2> /dev/null
-umount $partition3 2> /dev/null
-umount $partition4 2> /dev/null
+unmount $sd_device 2> /dev/null
 
-echo "mkfs.vfat -F 32 $partition1"
-mkfs.vfat -F 32 $partition1
-
-#echo "mkfs.ext2 $partition2"
-#mkfs.ext2 $partition2  
-
-#echo "mkfs.ext2 $partition3"
-#mkfs.ext2 $partition3  
-
-#echo "mkfs.ext2 $partition4"
-#mkfs.ext2 $partition4  
-
-####################################
-# mount 
-#umount /media/sd 2> /dev/null
-#mkdir -p /media/sd
-#echo "mount -t vfat $partition1 /media/sd"
-#mount -t vfat $partition1 /media/sd
+echo "mkfs.vfat -F 32 $sd_device"
+mkfs.vfat -F 32 $sd_device
 
 ####################################
 #<BL1 fusing>
@@ -83,14 +46,14 @@ bl1_position=1
 uboot_position=49
 
 echo "BL1 fusing"
-./mkbl1 ../uboot_inand.bin SD-bl1-8k.bin 8192
+./mkbl1 ../u-boot.bin SD-bl1-8k.bin 8192
 dd iflag=dsync oflag=dsync if=SD-bl1-8k.bin of=$1 seek=$bl1_position
 rm SD-bl1-8k.bin
 
 ####################################
 #<u-boot fusing>
 echo "u-boot fusing"
-dd iflag=dsync oflag=dsync if=../uboot_inand.bin of=$1 seek=$uboot_position
+dd iflag=dsync oflag=dsync if=../u-boot.bin of=$1 seek=$uboot_position
 
 ####################################
 #<Message Display>
